f3ff2bd898140b09e0b5bf57d80faecaff1d3593
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-05-15 17:02:26 -0300

    Create responsive sidebar component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,19 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Fernando Martins Pimenta</Text>
-        <Text color="gray.300" fontSize="small">
-          fernando.m.pimenta@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Fernando Martins Pimenta</Text>
+          <Text color="gray.300" fontSize="small">
+            fernando.m.pimenta@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Fernando Pimenta"
         src="https://github.com/pimentafm.png"
-      ></Avatar>
+      />
     </Flex>
   );
 }
