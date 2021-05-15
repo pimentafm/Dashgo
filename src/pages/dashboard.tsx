6c391faf12a8dce152a764c275f62b6b7d124a7a
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-05-15 14:58:29 -0300

    Set ApexOptions to apexcharts options

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,7 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 
+import { ApexOptions } from "apexcharts";
+
 import dynamic from "next/dynamic";
 
 import { Header } from "../components/Header";
@@ -7,7 +9,7 @@ import { Sidebar } from "../components/Sidebar";
 
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-const options = {
+const options: ApexOptions = {
   chart: {
     toolbar: {
       show: false,
