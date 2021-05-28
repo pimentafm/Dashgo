839d437c95ad54b528384ef8e330c71eb454e3b2
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-05-28 15:21:26 -0300

    Adds a spinner to the screen when refetch the users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -32,7 +32,7 @@ interface usersProps {
 }
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
       const response = await fetch("http://localhost:3000/api/users");
@@ -75,6 +75,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
