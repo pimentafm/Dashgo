dc694d6c8286cf53471de3f089ab709d0673f2b4
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-05-28 13:17:15 -0300

    Configure Yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,23 @@ import { Button, Flex, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 type SignInFormData = {
   email: string;
   password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("e-mail obrigatório").email("e-mail inválido"),
+  password: yup.string().required("senha obrigatória"),
+});
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -36,14 +46,14 @@ export default function SignIn() {
             type="email"
             label="e-mail"
             error={errors.email}
-            {...register("email", { required: "e-mail obrigatório" })}
+            {...register("email")}
           ></Input>
           <Input
             name="password"
             type="password"
             label="Senha"
             error={errors.password}
-            {...register("password", { required: "Senha obrigatória" })}
+            {...register("password")}
           ></Input>
         </Stack>
 
